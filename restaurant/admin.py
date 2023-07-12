82a8e3e1f9f57ea4da26defe58aaf55b540769a1
Max Bilokur <bilmaxim@gmail.com>
2023-07-12 20:09:01 +0300

    added check and printer models to admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from restaurant.models import Check, Printer
+
+
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "printer_id",
+        "type",
+        "order",
+        "status",
+        "pdf_file",
+    )
+    list_filter = (
+        "printer_id__name",
+        "type",
+        "status",
+    )
+
+
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "api_key",
+        "check_type",
+        "point_id",
+    )
