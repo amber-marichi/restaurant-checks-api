6082daa843b04b6a22a656a2bc8b6771df75b3a6
Max Bilokur <bilmaxim@gmail.com>
2023-07-21 13:23:04 +0300

    models updated

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,34 +1,44 @@
+import uuid
 from django.db import models
 
 
-PRINT_TYPE_CHOICES = [
-    ("CLT", "client"),
-    ("KIT", "kitchen"),
-]
+class PrintType(models.TextChoices):
+    CLIENT = "CLT"
+    KITCHEN = "KIT"
 
 
 class Printer(models.Model):
     name = models.CharField(max_length=200)
-    api_key = models.CharField(max_length=200, unique=True)
-    check_type = models.CharField(max_length=3, choices=PRINT_TYPE_CHOICES)
+    api_key = models.UUIDField(default=uuid.uuid4, unique=True)
+    check_type = models.CharField(max_length=3, choices=PrintType.choices)
     point_id = models.IntegerField()
 
     def __str__(self) -> str:
-        return self.name + self.check_type.value
+        return self.name + " - " + self.check_type
 
 
 class Check(models.Model):
+    NEW = "N"
+    READY = "R"
+    PRINTED = "P"
     CHECK_STATUS_CHOICES = [
-        ("N", "new"),
-        ("R", "ready"),
-        ("P", "printed")
+        (NEW, "new"),
+        (READY, "ready"),
+        (PRINTED, "printed")
     ]
 
-    printer_id = models.ForeignKey(to=Printer, on_delete=models.CASCADE)
-    order_type = models.CharField(max_length=3, choices=PRINT_TYPE_CHOICES)
-    oder = models.JSONField(blank=True)
-    status = models.CharField(max_length=1, choices=CHECK_STATUS_CHOICES)
-    pdf_file = models.FileField(blank=True, upload_to="pdf")
+    printer_id = models.ForeignKey(
+        to=Printer,
+        on_delete=models.CASCADE,
+        related_name="checks"
+    )
+    type = models.CharField(max_length=3, choices=PrintType.choices)
+    order = models.JSONField()
+    status = models.CharField(
+        max_length=1,
+        choices=CHECK_STATUS_CHOICES,
+        default=CHECK_STATUS_CHOICES[0][0])
+    pdf_file = models.FileField(upload_to='pdf/', blank=True)
 
     def __str__(self) -> str:
-        return str(self.id)
+        return str(self.id) + " - " + self.type
